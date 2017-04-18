439cc216d025f597a4dc042c33098d299862c465
Prateek Sharma <prateek_sharma_may@yahoo.com>
2017-04-17 17:21:31 -0700

    Final push with HTML and img webpack implementation

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@
  */
 var path = require("path");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
     filename: 'main.css'
@@ -11,8 +13,8 @@ module.exports = {
     entry: './src/js/app.js',
     output:{
         path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist'
+        filename: 'bundle.js'
+        /*publicPath: '/dist'*/
     },
     module:{
         rules: [
@@ -32,10 +34,31 @@ module.exports = {
                 use: extractPlugin.extract({
                     use: ['css-loader','sass-loader']
                 })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            /*publicPath: 'img/'*/
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins:[
-        extractPlugin
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
     ]
 }
